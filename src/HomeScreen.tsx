1393b72420809ab40f6a0ee078d012abc1d8597c
Ludwig <ludwig.pfann@brz.eu>
2025-07-06 15:46:49 +0200

    init

---
+++ src/HomeScreen.tsx
@@ -0,0 +1,19 @@
+﻿import {Text, View, StyleSheet} from "react-native";
+
+export function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Ein Buch über eine Indianer-Karte-Schatz</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
